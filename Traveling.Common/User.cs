3faf3c4f3367d4a5f710ce935ee2d4de58b41711
Sohaib Salman <sohaibsalman10@gmail.com>
2020-08-25 13:44:22 +0500

    Add User Class in Traveling.Common

---
+++ Traveling.Common/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traveling.Common
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
